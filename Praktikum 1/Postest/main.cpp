2410e93c3d7148a91e1dc5b0d9015437ffccedfd
Rafifah364 <raff2942@gmail.com>
2022-03-20 11:39:15 +0700

    membuat main-cpp

---
+++ Praktikum 1/Postest/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+using namespace std;
+int main(){
+    a:
+    system("color 0a");
+    cout<<"======================\n";
+    int pil,lagi;
+    float satu,dua;
+    cout<<"1. Penjumlahan\n";
+    cout<<"2. Pembagian\n";
+    cout<<"3. Perkalian\n";
+    cout<<"4. Pengurangan\n";
+    cout<<"Masukkan pilihan = "; cin>>pil;
+}
